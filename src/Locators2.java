6fbb353eeb9a64f7e5fbc32969381e4f84286721
Marc Borruey Hoyos <marc.borrueyhoyos@everis.nttdata.com>
2022-12-13 18:07:56 +0100

    dia 13 ntt

--- src/Locators2.java
+++ src/Locators2.java
@@ -8,18 +8,27 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Locators2 {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
 		// Similar al archivo Locators, probando cosas de distinta forma
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://sso.teachable.com/secure/9521/identity/login/password");
+		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
 		
-		driver.findElement(By.cssSelector("input[id='email']")).sendKeys("marc");
+		driver.findElement(By.cssSelector("input[id='username']")).sendKeys("marc");
 		driver.findElement(By.cssSelector("input[id='password']")).sendKeys("contraseñaaa");
 		
+		// Igual que a sobre. Al posar * permet buscar posant nomes les primeres lletres
+		//driver.findElement(By.cssSelector("input[id*='pass']")).sendKeys("contraseñaaa");
+		
+		// A partir de aquí espera a ejecutarse 2 segundos
+		// Sirve sobretodo si hay cosas de la página que nada más cargar aún no aparecen (un botón, ... Para mensajes de error, mejor usar implicitlyWait)
+		Thread.sleep(2000);
+		
 		// Si hay algo escrito en el input, lo borra
-		driver.findElement(By.cssSelector("input[id='email']")).clear();
+		driver.findElement(By.cssSelector("input[id='password']")).clear();
+		
+		
 		
 	
 	}
