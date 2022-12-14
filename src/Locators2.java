9521db55a7514cf68d06c48cea1311f765116b82
TheDarkLived <borruey93@gmail.com>
2022-12-14 18:05:06 +0100

    dia 14

--- src/Locators2.java
+++ src/Locators2.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 
 public class Locators2 {
 	
@@ -18,16 +19,28 @@ public class Locators2 {
 		driver.findElement(By.cssSelector("input[id='username']")).sendKeys("marc");
 		driver.findElement(By.cssSelector("input[id='password']")).sendKeys("contraseñaaa");
 		
+		// Se clica un checkbox
+		driver.findElement(By.id("terms")).click();
+		
 		// Igual que a sobre. Al posar * permet buscar posant nomes les primeres lletres
 		//driver.findElement(By.cssSelector("input[id*='pass']")).sendKeys("contraseñaaa");
 		
 		// A partir de aquí espera a ejecutarse 2 segundos
 		// Sirve sobretodo si hay cosas de la página que nada más cargar aún no aparecen (un botón, ... Para mensajes de error, mejor usar implicitlyWait)
-		Thread.sleep(2000);
 		
 		// Si hay algo escrito en el input, lo borra
 		driver.findElement(By.cssSelector("input[id='password']")).clear();
 		
+		// Si hay varios elementos que se llaman igual (por ejemplo 2 inputs y los 2 tienen como nombre de clase "checkmark"), puedes indicar a cual te refieres poniendo [] al final
+		driver.findElement(By.xpath("(//span[@class='checkmark'])[2]")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.xpath("(//button[@id='okayBtn'])")).click();
+		
+		
+		//Thread.sleep(7000);
+		//driver.findElement(By.xpath("//input[contains(@class,'btn')]")).click();
+		
+		//Assert.assertEquals(false, null);
 		
 		
 	
